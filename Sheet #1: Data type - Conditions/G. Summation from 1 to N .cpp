3cf23d942974aedb378c3e69bda18f4f341d6115
Hamza Kafafi <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-10 21:59:01 +0300

    Enhance comments in summation program
    Added comments to clarify the summation calculation and included a link for reference.

---
+++ Sheet #1: Data type - Conditions/G. Summation from 1 to N .cpp
@@ -0,0 +1,13 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+#include<iostream>
+using namespace std;
+int main()
+{
+long long  N;
+cin >> N;
+long long X= ( N * (N+1))/2; // هيحسب القوس الصغير ثم هيحسب الكبير ثم يقسم الكل علي 2 
+  
+cout << X << endl;
+ }
+// قاعدة تراكمية بحتة تانية ثانوي 
+// https://youtu.be/u50KdxBJFEw?si=R_bqiDCl3mj2G-bU&t=424
