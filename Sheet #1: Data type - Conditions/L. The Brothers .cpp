ed02c23b403e00dc7507186e617d5ead134e0d3c
Hamza Kafafi <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-11 12:45:15 +0300

    Add C++ program to check if two names are brothers

---
+++ Sheet #1: Data type - Conditions/L. The Brothers .cpp
@@ -0,0 +1,11 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+#include<iostream>
+using namespace std;
+int main()
+{
+string F1 , S1;
+string F2 , S2;
+cin >> F1 >> S1 >> F2 >> S2;
+if ( S1 == S2 ) {cout << "ARE Brothers" << endl;}
+else{cout << "NOT" << endl;}
+}
