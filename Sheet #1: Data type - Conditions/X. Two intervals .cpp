d3d476136601f003a564d1a77f20b0788f677b4e
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-13 10:53:30 +0300

    Add solution for interval overlap problem

---
+++ Sheet #1: Data type - Conditions/X. Two intervals .cpp
@@ -0,0 +1,16 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/X
+
+#include <iostream>
+using namespace std;
+int main() {
+    long long l1, r1, l2, r2;
+    cin >> l1 >> r1 >> l2 >> r2;
+    long long start = max(l1, l2);
+    long long end = min(r1, r2);
+    if (start <= end) {
+        cout << start << " " << end << endl;
+    } else {
+        cout << -1 << endl;
+    }
+    return 0;
+}
