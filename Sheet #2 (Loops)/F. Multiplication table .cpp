46a3a82a8f7c6638ba54e102d28098e4ac301a9c
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-15 14:31:43 +0300

    Add multiplication table program in C++

---
+++ Sheet #2 (Loops)/F. Multiplication table .cpp
@@ -0,0 +1,11 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/F
+#include<iostream>
+using namespace std;
+int main()
+{
+long long X; cin >> X;
+for(long long i =1; i <=12;i++ ){
+  long long m = X * i;
+cout << X << " * " << i << " = " << m <<endl;
+}
+ }
