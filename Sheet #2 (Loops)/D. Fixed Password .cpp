5d2c8ac4f5b6962e9e6ab05cc77aa6ac75d7b424
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-15 14:28:55 +0300

    Add Fixed Password program in C++

---
+++ Sheet #2 (Loops)/D. Fixed Password .cpp
@@ -0,0 +1,9 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+#include<iostream>
+using namespace std;
+int main()
+{
+for (long long X;cin >> X; cout << "Wrong" << endl) {if (X==1999)
+break;}
+cout << "Correct";
+}
