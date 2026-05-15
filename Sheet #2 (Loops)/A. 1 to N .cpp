69cb3960b44d076dee2adb98ae0ead9625f3030c
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-15 14:23:53 +0300

    Add C++ program to print numbers from 1 to N

---
+++ Sheet #2 (Loops)/A. 1 to N .cpp
@@ -0,0 +1,10 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/A
+#include<iostream>
+using namespace std;
+int main()
+{
+long long X;
+cin >> X;
+for(long long i=1;i<=X;i++)
+cout << i << endl;
+}
