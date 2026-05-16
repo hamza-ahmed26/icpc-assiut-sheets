cb0072ae8eb8196c85a7eba2265983792335033e
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-16 12:27:10 +0300

    Add K. Divisors program to find divisors of n

---
+++ Sheet #2 (Loops)/K. Divisors .cpp
@@ -0,0 +1,19 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/K
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n;
+    long long i ;
+    cin >> n;
+long long j;
+
+for (long long i = 1; i <= n; i++)
+{
+
+if (n%i == 0)
+    cout << i << endl ;
+}
+
+
+}
