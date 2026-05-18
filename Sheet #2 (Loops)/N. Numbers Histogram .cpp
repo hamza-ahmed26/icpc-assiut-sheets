3e4c906090d7f9e767eb9249d650a4577c80d648
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-18 21:07:48 +0300

    Add N. Numbers Histogram program

---
+++ Sheet #2 (Loops)/N. Numbers Histogram .cpp
@@ -0,0 +1,21 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/N
+#include <iostream>
+using namespace std;
+int main() {
+ char a;
+    cin >> a;
+    long long b;
+    cin >> b;
+    long long x= 0;
+    for (long long i = 0; i < b; i++)
+    {
+        cin >> x;
+        
+     for (long long j = 0; j < x; j++)
+     {
+        cout << char(a);
+    
+     }
+     cout << endl;
+    }   
+    }
