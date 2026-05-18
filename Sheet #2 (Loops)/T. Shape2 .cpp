68439c2bca9a88df6d4cd4ea73cc5d46ad06b31c
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-18 21:15:57 +0300

    Add T. Shape2.cpp for pyramid pattern printing

---
+++ Sheet #2 (Loops)/T. Shape2 .cpp
@@ -0,0 +1,19 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/T
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 0; k < (2 * i - 1); k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
