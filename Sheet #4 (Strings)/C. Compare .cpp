fd91c28634d9e69c0be534f9d51b6899ee43de38
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-19 12:53:27 +0300

    Add C. Compare program to compare two strings

---
+++ Sheet #4 (Strings)/C. Compare .cpp
@@ -0,0 +1,14 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/C
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string x, y;
+    cin >> x >> y;
+    if (x <= y) {
+        cout << x << endl;
+    } else {
+        cout << y << endl;
+    }
+    return 0;
+}
