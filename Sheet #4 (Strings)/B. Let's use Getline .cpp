55af203870ead5cb9129a467869bbcd2db9b929f
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-19 12:52:11 +0300

    Add getline example with backslash termination
    Implement getline to read a string and output characters until a backslash is encountered.

---
+++ Sheet #4 (Strings)/B. Let's use Getline .cpp
@@ -0,0 +1,17 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/B
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+string s;
+getline(cin, s);
+for (int i = 0; i < s.length(); i++)
+{if (s[i] == '\\')
+{
+  break;
+}
+cout << s[i] ;
+}
+return 0;
+}
