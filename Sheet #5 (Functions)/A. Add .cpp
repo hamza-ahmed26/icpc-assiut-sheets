e798fed3764c38e5c0dba5763b70bad23025a236
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-25 11:47:47 +0300

    Add A. Add.cpp for summing two integers

---
+++ Sheet #5 (Functions)/A. Add .cpp
@@ -0,0 +1,17 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/A
+#include <iostream>
+#include<cmath>
+using namespace std;
+ int sum (int x,int y)
+ {
+    return x +y  ;
+
+ }
+int main()
+{
+    int x , y ;
+    cin >> x >> y ;
+  long long r = sum(x,y);
+  cout<<r<<endl;
+    return 0;
+}
