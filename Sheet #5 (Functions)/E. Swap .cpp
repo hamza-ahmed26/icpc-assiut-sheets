5d798c1988b2ec78a40c873ab4496c05e40c0938
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-05-25 12:07:22 +0300

    Add E. Swap program to swap two integers

---
+++ Sheet #5 (Functions)/E. Swap .cpp
@@ -0,0 +1,17 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/E
+#include <iostream>
+#include<cmath>
+using namespace std;
+ void sum (int y , int x)
+ { 
+        cout << y << " " <<  x ;     
+             
+    }
+int main()
+{
+    int x , y;
+    cin >> x >> y;
+    sum(y,x);
+  
+    return 0;
+}
